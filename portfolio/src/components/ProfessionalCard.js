1d9b28ae3efb68158c72f6938b902ee5d2d8f729
Thibault-Barbe <thibault.barbe@epitech.eu>
2024-10-18 19:15:45 +0200

    Responsiveness is finished

--- portfolio/src/components/ProfessionalCard.js
+++ portfolio/src/components/ProfessionalCard.js
@@ -1,41 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ProfessionnalCard = ({ company, logo, startDate, endDate, job, description, skills, skillsLogo, companySite, data }) => {
+const ProfessionalCard = ({ company, logo, startDate, endDate, job, description, skills, skillsLogo, companySite, data }) => {
     return (
-        <div className="w-[30%] h-[800px] max-w-4xl mx-auto bg-[#2B2B2D] rounded-xl shadow-xl shadow-black border border-gray-700 mb-6 hover:scale-110 duration-300">
-          <div className="flex flex-col items-center">
-            <div className="w-full h-[20%]">
-                <img src={logo} className="w-[100%] rounded-t-xl" />
-            </div>
-            <div className="p-6 flex flex-col items-center p-6">
-                <Link href={companySite} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:underline">{company}</Link>
-                <h3 className="font-bold lg:text-[20px] text-[25px] text-white uppercase">{job}</h3>
-                <div className="text-right">
-                <p className="text-gray-500 text-sm">
-                    {startDate} - {endDate || 'Present'}
-                </p>
-                </div>
-            </div>
+      <div className="w-full lg:w-[30%] mx-auto bg-[#2B2B2D] rounded-xl shadow-xl shadow-black border border-gray-700 mb-6 hover:scale-105 lg:hover:scale-110 duration-300">
+        <div className="flex flex-col items-center">
+          <div className="w-full h-[200px]">
+            <img src={logo} className="w-full h-full object-cover rounded-t-xl" alt={company} />
           </div>
-          <div className="mt-4 p-6">
-            <p className="text-gray-300 text-justify">{description}</p>
-          </div>
-          <div className="mt-4 flex flex-col items-center">
-            <h2 className="text-md font-semibold text-white mb-6">{data.keySkills}</h2>
-            <ul className="flex flex-row w-full justify-around items-center">
-              {skills.map((skill, index) => (
-                <div key={index + "-div"} className="flex flex-col items-center space-y-2">
-                    <div key={index + "-div-div"} className="border-[3px] border-gray-200 rounded-full w-[70px] h-[70px] flex items-center justify-center">
-                        <Image key={index} src={skillsLogo[index]} width={50} height={50} alt={skill} />
-                    </div>
-                    <li key={index}>{skill}</li>
-                </div>
-              ))}
-            </ul>
+          <div className="p-6 flex flex-col items-center justify-center w-full">
+            <Link href={companySite} target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:underline">
+              {company}
+            </Link>
+            <h3 className="font-bold text-[16px] md:[20px] text-white uppercase">{job}</h3>
+            <p className="text-gray-500 text-sm">
+              {startDate} - {endDate || 'Present'}
+            </p>
           </div>
         </div>
-      );
+
+        <div className="mt-4 p-6">
+          <p className="text-gray-300 text-justify">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-col items-center mb-3">
+          <h3 className="text-md font-semibold text-white mb-6 text-[15px] md:text-[25px]">{data.keySkills}</h3>
+          <ul className="flex flex-row w-full justify-around items-center">
+            {skills.map((skill, index) => (
+              <div key={index + "-div"} className="flex flex-col items-center space-y-2">
+                <div className="border-[3px] border-gray-200 rounded-full w-[70px] h-[70px] flex items-center justify-center">
+                  <Image key={index} src={skillsLogo[index]} width={50} height={50} className="p-1" alt={skill} />
+                </div>
+                <li key={index} className="text-gray-300">{skill}</li>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
 }
 
-export default ProfessionnalCard;
+export default ProfessionalCard;
--- portfolio/src/components/ProjectCard.js
+++ portfolio/src/components/ProjectCard.js
@@ -5,131 +5,136 @@ import { AiOutlineExport } from "react-icons/ai";
 
 const LargeProjectCard = ({ title, subtitle, description, skills, gihtubLink, productionLink, image, index }) => {
     return index % 2 === 0 ? (
-        <div className="justify-between flex flex-row">
-            <div className="flex flex-col space-y-10 justify-between w-[50%]">
-                <div className="flex flex-col">
-                    <h3>{title}</h3>
-                    <h4 className="text-gray-400">{subtitle}</h4>
-                </div>
-                <div className="text-justify rounded-xl shadow-sm shadow-black bg-[#2B2B2D] p-8">
-                    <p>{description}</p>
-                </div>
-                <div className="text-[14px] space-x-4 flex flex-row">
-                    { skills.map((skill, index) => (
-                        <p key={index}>{skill}</p>
-                    ))}
-                </div>
-                <div className="flex flex-row space-x-4 items-center">
-                    {gihtubLink !== "" && <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
-                        <FaGithub className="h-[40px] w-[40px] text-black cursor-pointer hover:text-gray-200" />
-                    </Link>}
-                    {productionLink !== "" && <Link href={productionLink} target="_blank" rel="noopener noreferrer">
-                        <AiOutlineExport className="h-[37px] w-[37px] text-black hover:text-gray-200 cursor-pointer" />
-                    </Link>}
-                </div>
-            </div>
-            <div className="w-[45%] rounded-2xl">
-                <img src={image} className="flex-1" alt={title} />
-            </div>
+      <div className="flex flex-col lg:flex-row justify-between space-y-4 lg:space-y-0 w-full">
+        <div className="flex flex-col space-y-6 lg:space-y-10 justify-between w-full lg:w-[50%]">
+          <div className="flex flex-col">
+            <h3 className="text-[22px] md:text-[26px]">{title}</h3>
+            <h4 className="text-gray-400">{subtitle}</h4>
+          </div>
+          <div className="text-justify bg-[#2B2B2D] rounded-xl shadow-sm p-6 lg:p-8">
+            <p>{description}</p>
+          </div>
+          <div className="flex flex-wrap text-[14px] space-x-4">
+            {skills.map((skill, index) => (
+              <p key={index}>{skill}</p>
+            ))}
+          </div>
+          <div className="flex flex-row space-x-4 items-center">
+            {gihtubLink && <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
+              <FaGithub className="h-[40px] w-[40px] text-black cursor-pointer hover:text-gray-200" />
+            </Link>}
+            {productionLink && <Link href={productionLink} target="_blank" rel="noopener noreferrer">
+              <AiOutlineExport className="h-[37px] w-[37px] text-black cursor-pointer hover:text-gray-200" />
+            </Link>}
+          </div>
         </div>
+        <div className="w-full lg:w-[45%] rounded-2xl items-center flex">
+          <img src={image} className="w-full h-auto object-cover rounded-2xl object-cover" alt={title} />
+        </div>
+      </div>
     ) : (
-        <div className="justify-between flex flex-row">
-            <div className="w-[45%] rounded-2xl">
-                <img src={image} className="flex-1" alt={title} />
-            </div>
-            <div className="flex flex-col space-y-10 justify-between items-end w-[45%]">
-                <div className="flex flex-col items-end">
-                    <h3>{title}</h3>
-                    <h4 className="text-gray-400">{subtitle}</h4>
-                </div>
-                <div className="text-justify rounded-xl shadow-sm shadow-black bg-[#2B2B2D] p-8">
-                    <p>{description}</p>
-                </div>
-                <div className="text-[14px] space-x-4 flex flex-row">
-                    { skills.map((skill, index) => (
-                        <p key={index}>{skill}</p>
-                    ))}
-                </div>
-                <div className="flex flex-row space-x-4 items-center">
-                    {productionLink !== "" && <Link href={productionLink} target="_blank" rel="noopener noreferrer">
-                        <AiOutlineExport className="h-[37px] w-[37px] text-black hover:text-gray-200 cursor-pointer" />
-                    </Link>}
-                    {gihtubLink !== "" && <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
-                        <FaGithub className="h-[40px] w-[40px] text-black cursor-pointer hover:text-gray-200" />
-                    </Link>}
-                </div>
-            </div>
+      <div className="flex flex-col lg:flex-row-reverse justify-between space-y-4 lg:space-y-0 w-full">
+        <div className="flex flex-col space-y-6 lg:space-y-10 justify-between w-full lg:w-[50%]">
+          <div className="flex flex-col items-end">
+            <h3 className="text-[22px] md:text-[26px]">{title}</h3>
+            <h4 className="text-gray-400">{subtitle}</h4>
+          </div>
+          <div className="text-justify bg-[#2B2B2D] rounded-xl shadow-sm p-6 lg:p-8">
+            <p>{description}</p>
+          </div>
+          <div className="flex flex-wrap text-[14px] space-x-4 justify-end">
+            {skills.map((skill, index) => (
+              <p key={index}>{skill}</p>
+            ))}
+          </div>
+          <div className="flex flex-row space-x-4 items-center justify-end">
+            {productionLink && <Link href={productionLink} target="_blank" rel="noopener noreferrer">
+              <AiOutlineExport className="h-[37px] w-[37px] text-black cursor-pointer hover:text-gray-200" />
+            </Link>}
+            {gihtubLink && <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
+              <FaGithub className="h-[40px] w-[40px] text-black cursor-pointer hover:text-gray-200" />
+            </Link>}
+          </div>
         </div>
-    )
-}
+        <div className="w-full lg:w-[45%] rounded-2xl items-center flex">
+          <img src={image} className="w-full h-auto object-contain rounded-2xl" alt={title} />
+        </div>
+      </div>
+    );
+  };
 
-const MediumProjectCard = ({ title, subtitle, description, skills, gihtubLink, progress }) => {
+  const MediumProjectCard = ({ title, subtitle, description, skills, gihtubLink, progress }) => {
     return (
-        <div className="justify-between space-y-4 p-6 rounded-xl shadow-2xl shadow-black w-[30%] flex flex-col items-center bg-[#2B2B2D] hover:scale-105 transform duration-300">
-            <div className="flex flex-row w-full justify-between items-center">
-                <div className="flex flex-col">
-                    <h3>{title}</h3>
-                    <h4 className="text-gray-400">{subtitle}</h4>
-                </div>
-                <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
-                    <FaGithub className="h-[25px] w-[25px] text-black cursor-pointer" />
-                </Link>
-            </div>
-            <p className="text-justify">{description}</p>
-            <div className="flex flex-row justify-between w-full items-center">
-                <div className="text-[14px] space-x-4 flex flex-row">
-                    { skills.map((skill, index) => (
-                        <p key={index}>{skill}</p>
-                    ))}
-                </div>
-                <div className="relative flex items-center justify-center h-[60px] w-[60px]">
-                    <svg className="absolute transform -rotate-50" viewBox="0 0 36 36">
-                        <path
-                        className="text-gray-300"
-                        d={`M18 2.0845
-                            a 15.9155 15.9155 0 0 1 0 31.831
-                            a 15.9155 15.9155 0 0 1 0 -31.831`}
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                        />
-                        <path
-                        className="text-green-500"
-                        strokeDasharray={`${progress}, 100`} // Dynamically set the progress
-                        d={`M18 2.0845
-                            a 15.9155 15.9155 0 0 1 0 31.831
-                            a 15.9155 15.9155 0 0 1 0 -31.831`}
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                        />
-                    </svg>
-                    <div className="absolute text-[15px] font-semibold text-white">
-                        {progress}%
-                    </div>
-                </div>
-            </div>
+      <div className="flex flex-col justify-between space-y-4 p-6 rounded-xl shadow-2xl shadow-black bg-[#2B2B2D] hover:scale-105 transform duration-300 w-full sm:w-[48%] lg:w-[30%] items-start">
+        {/* Title and GitHub Link */}
+        <div className="flex flex-row w-full justify-between items-center">
+          <div className="flex flex-col">
+            <h3 className="text-[16px] sm:text-[18px] md:text-[22px]">{title}</h3>
+            <h4 className="text-gray-400 text-[14px] sm:text-[16px]">{subtitle}</h4>
+          </div>
+          <Link href={gihtubLink} target="_blank" rel="noopener noreferrer">
+            <FaGithub className="h-[20px] w-[20px] sm:h-[25px] sm:w-[25px] text-black cursor-pointer" />
+          </Link>
         </div>
-    )
-}
+
+        {/* Description */}
+        <p className="text-justify text-[14px] sm:text-[16px] md:text-[18px]">{description}</p>
+
+        {/* Skills and Progress Bar */}
+        <div className="flex flex-row justify-between w-full items-center">
+          <div className="flex flex-wrap gap-2 sm:gap-4 text-[12px] sm:text-[14px] w-full">
+            {skills.map((skill, index) => (
+              <p key={index} className="bg-gray-700 p-1 rounded">{skill}</p>
+            ))}
+          </div>
+          <div className="relative flex items-center justify-center h-[40px] w-[40px] sm:h-[60px] sm:w-[60px]">
+            <svg className="absolute transform -rotate-50" viewBox="0 0 36 36">
+              <path
+                className="text-gray-300"
+                d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+              <path
+                className="text-green-500"
+                strokeDasharray={`${progress}, 100`}
+                d="M18 2.0845
+                    a 15.9155 15.9155 0 0 1 0 31.831
+                    a 15.9155 15.9155 0 0 1 0 -31.831"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="4"
+              />
+            </svg>
+            <div className="absolute text-[12px] sm:text-[15px] font-semibold text-white">
+              {progress}%
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
 
 const SmallProjectCard = ({ title, subtitle, description, skills, startDate }) => {
     return (
-        <div className="w-[90%] space-y-3 rounded-2xl shadow-2xl shadow-black p-4 bg-[#2B2B2D] hover:scale-105 transform duration-300">
-            <div>
-                <h3>{title}</h3>
-                <h4 className="text-gray-400">{subtitle}</h4>
-            </div>
-            <p>{description}</p>
-            <p>{startDate}</p>
-            <div className="text-[14px] space-x-4 flex flex-row">
-                { skills.map((skill, index) => (
-                    <p key={index}>{skill}</p>
-                ))}
-            </div>
+      <div className="w-full sm:w-[90%] md:w-[70%] lg:w-[60%] space-y-3 rounded-2xl shadow-2xl shadow-black p-4 bg-[#2B2B2D] hover:scale-105 transform duration-300">
+        <div>
+          <h3 className="text-[18px] md:text-[20px]">{title}</h3>
+          <h4 className="text-gray-400 text-[16px]">{subtitle}</h4>
         </div>
-    )
-}
+        <p className="text-[16px] md:text-[18px]">{description}</p>
+        <p className="text-gray-400">{startDate}</p>
+        <div className="flex flex-wrap gap-2 sm:gap-4 text-[12px] sm:text-[14px] w-full">
+            {skills.map((skill, index) => (
+              <p key={index} className="bg-gray-700 p-1 rounded">{skill}</p>
+            ))}
+          </div>
+      </div>
+    );
+  };
 
 const ProjectCard = ({ data, size, index }) => {
     switch (size) {
